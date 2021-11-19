20b30a0bcd7717dd43659ca19f1c46a2cf733767
lauaviin <j.tihvan@gmail.com>
2021-11-19 21:17:06 +0200

    feat: barchart component

---
+++ web/src/components/BarChart/BarChart.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "button": string;
+  readonly "hello": string;
+};
+export = styles;
+
---
+++ web/src/components/BarChart/BarChart.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const BarChart: React.FunctionComponent = () => {
+  return (
+    <div>
+      BarChart!
+    </div>
+  );
+};
