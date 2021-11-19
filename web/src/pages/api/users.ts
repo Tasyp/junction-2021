4b3ef53a9854f38bc89fc31bcdb442c5b02bf9b2
Risto Novik <est.risto@gmail.com>
2021-11-19 21:15:09 +0200

    fix: API endoint

---
+++ web/src/pages/api/users.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(_request: NextApiRequest, response: NextApiResponse) {
+  response.status(200).json({ user: "Five Fold" });
+}
