241272ef38522a3c894bb7596aac1f49382c66a9
lauaviin <j.tihvan@gmail.com>
2021-11-19 21:14:30 +0200

    keeks

---
+++ web/src/pages/badges/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ExampleContainer} from "../../containers";
+
+
+const IndexPage: React.FunctionComponent = () => (
+    <ExampleContainer />
+);
+
+export default IndexPage;
