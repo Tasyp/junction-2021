1f3d2be72755a0cfc0d11ffd105c595b9b265149
Risto Novik <est.risto@gmail.com>
2021-11-19 21:58:58 +0200

    fix: Apartments endpoint

--- web/src/pages/api/users.ts
+++ web/src/pages/api/users.ts
@@ -1,10 +1,7 @@
-import path from 'path';
-import { readFileSync } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const orasDataDirPath = path.join('public', 'raw-oras-data.json');
-const orasDataDB = readFileSync(orasDataDirPath, 'utf-8');
-
 export default function handler(_request: NextApiRequest, response: NextApiResponse) {
-  response.status(200).json(orasDataDB);
+  response
+    .status(200)
+    .json({ user: 'Diesel' });
 }
