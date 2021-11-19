d0421b512e6fe195ca68d12b70126e064fc5ebd3
Risto Novik <est.risto@gmail.com>
2021-11-19 21:32:32 +0200

    feat: Add raw json data

--- web/src/pages/api/users.ts
+++ web/src/pages/api/users.ts
@@ -1,5 +1,10 @@
+import path from 'path';
+import { readFileSync } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const orasDataDirPath = path.join('public', 'raw-oras-data.json');
+const orasDataDB = readFileSync(orasDataDirPath, 'utf-8');
+
 export default function handler(_request: NextApiRequest, response: NextApiResponse) {
-  response.status(200).json({ user: "Five Fold" });
+  response.status(200).json(orasDataDB);
 }
