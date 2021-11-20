79d43199272eb1dcc7405b34e31eb1ae35034030
Risto Novik <est.risto@gmail.com>
2021-11-20 14:24:58 +0200

    feat: Achievement page

---
+++ web/src/lib/util.ts
@@ -0,0 +1,2 @@
+
+export const takeFirst = <T>(a: T[] | T): T => Array.isArray(a) ? a[0] : a;
---
+++ web/src/pages/achievement/[apartmentID].tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Apartment, fetchApartment } from "../../lib/api";
+import { NextPageContext } from "next";
+import { takeFirst } from "../../lib/util";
+
+interface Props {
+  apartment: Apartment;
+}
+
+function AchievementPage({ apartment }: Props) {
+  return (
+    <>
+      <h1>Achievements of #{apartment.apartmentID}</h1>
+      <div>{apartment.badges.length}</div>
+    </>
+  );
+}
+
+export async function getServerSideProps({ query }: NextPageContext) {
+  const { apartmentID } = query;
+
+  const data = await fetchApartment(takeFirst(apartmentID));
+  return {
+    props: { apartment: data.apartment },
+  };
+}
+
+
+export default AchievementPage;
