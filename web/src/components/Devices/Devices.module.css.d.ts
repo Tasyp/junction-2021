f44dfcf10fec8a0b5d5babd943accb7269799569
lauaviin <j.tihvan@gmail.com>
2021-11-20 20:32:15 +0200

    feat: myConsumption

--- web/src/components/Devices/Devices.module.css.d.ts
+++ web/src/components/Devices/Devices.module.css.d.ts
@@ -1,12 +1,10 @@
 declare const styles: {
-  readonly "container": string;
-  readonly "device": string;
-  readonly "deviceName": string;
-  readonly "progressContainer": string;
-  readonly "amount": string;
-  readonly "progress": string;
-  readonly "progressBar": string;
-  readonly "indexCaption": string;
+  readonly container: string;
+  readonly device: string;
+  readonly deviceName: string;
+  readonly progressContainer: string;
+  readonly amount: string;
+  readonly progress: string;
+  readonly progressBar: string;
 };
 export = styles;
-
